ef39b1d5c989ecaec5b366047c07a92fb608e7ee
Ivan Vargas <ivan4ndres.00@gmail.com>
2025-06-13 02:08:44 -0500

    fix: merge declarations functions

--- src/scripts/declarations.ts
+++ src/scripts/declarations.ts
@@ -86,3 +86,33 @@ export default function main(args: any[]) {
     if (err instanceof Error) throw new Error(`Error writing file "${outFile}": ${err.message}`);
   }
 }
+
+const declarations = new Set<string>();
+
+interface Params {
+  outFile?: string;
+  symbolName?: string;
+  remove?: boolean | string;
+  ts?: boolean | string;
+}
+
+export async function generateDeclaration(file: string, params: Params = {}): Promise<void> {
+  if (declarations.has(file)) return;
+  declarations.add(file);
+  let { outFile, symbolName, remove, ts } = params;
+  outFile &&= `--outFile=${outFile}`;
+  symbolName &&= `--symbolName=${symbolName}`;
+  remove &&= `--remove`;
+  ts &&= `--ts`;
+  try {
+    await main([!0, "declarations", file, outFile, symbolName, remove, ts].filter(Boolean));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function generateDeclarations(filesOrFolder: string[] | string, params: Params = {}) {
+  if (filesOrFolder instanceof Array) {
+    for (const file of filesOrFolder) await generateDeclaration(file, params);
+  } else await generateDeclaration(filesOrFolder, params);
+}
