a2049b4c086dfcf8103044897a92e16852a6017b
Ivan Vargas <ivan4ndres.00@gmail.com>
2025-06-19 11:04:27 -0500

    change default get locale fn

--- src/next/translation.ts
+++ src/next/translation.ts
@@ -3,13 +3,13 @@ import { TranslationNode } from "../react/translation";
 import { TranslationProvider, getTranslation } from "./rsc";
 import { isRSC } from "./state";
 import type { TranslationFC } from "../types";
-import { getCachedRequestLocale, setCachedRequestLocale } from "./cache";
+import { getRequestLocale, setRequestLocale } from "./request";
 
 if (isRSC) {
   TranslationNode.Provider = TranslationProvider as TranslationFC;
   TranslationNode.hook = getTranslation;
-  TranslationNode.setLocale = setCachedRequestLocale as any;
-  TranslationNode.getLocale = getCachedRequestLocale;
+  TranslationNode.setLocale = setRequestLocale as any;
+  TranslationNode.getLocale = getRequestLocale;
 }
 
 export { createTranslation, Translation, TranslationNode } from "../react/translation";
