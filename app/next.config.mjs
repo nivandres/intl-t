f00cbcae644c32e3404e5f851a560dbeeec91d40
Ivan Vargas <ivan4ndres.00@gmail.com>
2025-08-18 22:22:05 -0500

    feat: twoslash fixes

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -5,7 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  serverExternalPackages: ["typescript", "twoslash", "shiki", "ts-morph", "oxc-transform", "intl-t"],
+  serverExternalPackages: ["typescript", "twoslash"],
   images: {
     remotePatterns: [
       { hostname: "raw.githubusercontent.com" },
